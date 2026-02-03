afcf77fe07136369e5f3eadbf33a6ff2c32c8fd2
Vitolop1 <lopresttivito@gmail.com>
2026-02-03 16:13:36 -0500

    /

--- app/public/index.php
+++ app/public/index.php
@@ -2,7 +2,10 @@
 // index.php
 // Cars DB - Add, Search, Delete, Subquery
 
-require_once("login.php");
+require_once(__DIR__ . "/../config/login.php");
+
+// resto del código
+
 
 function h($str) {
     return htmlspecialchars((string)$str, ENT_QUOTES, 'UTF-8');
